534aeeb53bf8841773d036000a3296948bc5c7e5
Michael <michael.w.kolb@gmail.com>
2022-10-30 19:04:34 -0400

    Completed assignment

---
+++ app/serializers/author_posts_serializer.rb
@@ -0,0 +1,10 @@
+class AuthorPostsSerializer < ActiveModel::Serializer
+  attributes :title, :short_content, :tags
+
+  def short_content
+    "#{self.object.content[0..39]}..."
+  end
+
+  belongs_to :author
+  has_many :tags
+end
